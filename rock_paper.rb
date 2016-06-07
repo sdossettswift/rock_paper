75d8d44339c667aeb212f343e4bbc7487969831b
Dossett Swift <sdossettswift@gmail.com>
2016-06-07 14:37:22 -0500

    Update rock_paper.rb
    game works

--- rock_paper.rb
+++ rock_paper.rb
@@ -1,5 +1,44 @@
 class Game
-  attr_accessor :player_one_choice, :computer
+  attr_accessor :player_one_choice, :computer, :winner
+
+  def play
+    puts "Let's Play!"
+    puts "Choose (R)ock, (P)aper, or (S)cissors"
+    @player_one_choice = gets.chomp.upcase
+    @computer=["R", "P", "S"].sample
+
+    winner = nil
+
+    if @player_one_choice == @computer
+      winner = "TIE"
+    end
+
+    if @player_one_choice == "P" && @computer == "R"
+      winner = "PLAYER WINS. COMPUTER LOSES."
+    end
+
+    if @player_one_choice == "P" && @computer == "S"
+      winner = "COMPUTER WINS. PLAYER LOSES."
+    end
+
+    if @player_one_choice == "R" && @computer == "P"
+      winner = "COMPUTER WINS. PLAYER LOSES."
+    end
+
+    if @player_one_choice == "R" && @computer == "S"
+      winner = "PLAYER WINS. COMPUTER LOSES."
+    end
+    if @player_one_choice == "S" && @computer == "R"
+      winner = "COMPUTER WINS. PLAYER LOSES."
+    end
+
+    if @player_one_choice == "S" && @computer == "P"
+      winner = "PLAYER WINS. COMPUTER LOSES."
+    end
+    puts "#{winner}"
+
+  end
+
 end
 
 game = Game.new
